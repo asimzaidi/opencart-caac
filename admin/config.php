498d057e383819a123063569bae8089763b4ff10
asimzaidi <asim@wadic.net>
2013-01-05 12:51:14 -0800

    first commit to canadian accessories

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/canadianaccessories.ca/admin/');
+define('HTTP_CATALOG', 'http://localhost/canadianaccessories.ca/');
+define('HTTP_IMAGE', 'http://localhost/canadianaccessories.ca/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/canadianaccessories.ca/admin/');
+define('HTTPS_CATALOG', 'http://localhost/canadianaccessories.ca/');
+define('HTTPS_IMAGE', 'http://localhost/canadianaccessories.ca/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/canadianaccessories.ca/admin/');
+define('DIR_SYSTEM', '/var/www/canadianaccessories.ca/system/');
+define('DIR_DATABASE', '/var/www/canadianaccessories.ca/system/database/');
+define('DIR_LANGUAGE', '/var/www/canadianaccessories.ca/admin/language/');
+define('DIR_TEMPLATE', '/var/www/canadianaccessories.ca/admin/view/template/');
+define('DIR_CONFIG', '/var/www/canadianaccessories.ca/system/config/');
+define('DIR_IMAGE', '/var/www/canadianaccessories.ca/image/');
+define('DIR_CACHE', '/var/www/canadianaccessories.ca/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/canadianaccessories.ca/download/');
+define('DIR_LOGS', '/var/www/canadianaccessories.ca/system/logs/');
+define('DIR_CATALOG', '/var/www/canadianaccessories.ca/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'boat.ramp');
+define('DB_DATABASE', 'canadianaccessories');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/canadianaccessories.ca/');
+define('HTTP_IMAGE', 'http://localhost/canadianaccessories.ca/image/');
+define('HTTP_ADMIN', 'http://localhost/canadianaccessories.ca/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/canadianaccessories.ca/');
+define('HTTPS_IMAGE', 'http://localhost/canadianaccessories.ca/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/canadianaccessories.ca/catalog/');
+define('DIR_SYSTEM', '/var/www/canadianaccessories.ca/system/');
+define('DIR_DATABASE', '/var/www/canadianaccessories.ca/system/database/');
+define('DIR_LANGUAGE', '/var/www/canadianaccessories.ca/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/canadianaccessories.ca/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/canadianaccessories.ca/system/config/');
+define('DIR_IMAGE', '/var/www/canadianaccessories.ca/image/');
+define('DIR_CACHE', '/var/www/canadianaccessories.ca/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/canadianaccessories.ca/download/');
+define('DIR_LOGS', '/var/www/canadianaccessories.ca/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'boat.ramp');
+define('DB_DATABASE', 'canadianaccessories');
+define('DB_PREFIX', '');
+?>
